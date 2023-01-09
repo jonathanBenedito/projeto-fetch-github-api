b6fbd2ab8e01f37c24792c29450786a74adb2fb0
PatchesTheWright <jonathan.valbe.jb@gmail.com>
2023-01-09 09:45:29 -0300

    Uploading finished files to repository

---
+++ src/scripts/index.js
@@ -0,0 +1,46 @@
+import { getUser } from "./services/user.js"
+import { getRepositories } from "./services/repositories.js"
+
+import { user } from "./objects/user.js"
+import { screen } from "./objects/screen.js"
+
+document.getElementById('btn-search').addEventListener('click', () => {
+    const userName = document.getElementById('input-search').value
+    if(validateEmptyInput(userName)) return
+    getUserData(userName)
+})
+
+function validateEmptyInput(userName) {
+    if(userName.length === 0) {
+        alert('Preencha o campo com o nome do usuário do GitHub')
+        return true
+    }
+}
+
+document.getElementById('input-search').addEventListener('keyup', (e) => {
+    const userName = e.target.value
+    const key = e.which || e.keyCode
+    const isEnterKeyPressed = key === 13
+
+    if(isEnterKeyPressed) {
+        if(validateEmptyInput(userName)) return
+        getUserData(userName)
+    }
+})
+
+async function getUserData(userName) {
+
+    const userReponse = await getUser(userName)
+    
+    if(userReponse.message === "Not Found"){
+        screen.renderNotFound()
+        return
+    }
+
+    const repositoriesResponse = await getRepositories(userName)
+
+    user.setInfo(userReponse)
+    user.setRepositories(repositoriesResponse)
+
+    screen.renderUser(user)
+}
